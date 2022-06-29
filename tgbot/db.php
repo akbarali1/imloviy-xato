30789e941cee04b2160d769901c135fd04284185
Akbarali <Akbarali9705@mail.ru>
2022-06-29 21:29:26 +0500

    update

--- tgbot/db.php
+++ tgbot/db.php
@@ -6,15 +6,15 @@
 $mysqli = new Mysqli('localhost', 'ajax', '', 'ajax');
 $mysqli->set_charset("utf8");
 
-$url = trim($_POST['url_address']);
-$hato_soz = trim($_POST['error_text']);
+$url       = trim($_POST['url_address']);
+$hato_soz  = trim($_POST['error_text']);
 $tuzailgan = trim($_POST['fixed_text']);
 
-if($url && $hato_soz && $tuzailgan){
-$mysqli->query("INSERT INTO users (url,hato_soz,tuzailgan) VALUES('$url', '$hato_soz', '$tuzailgan')");
-$out = array( 'success' => 'true', 'message' => 'ordam berganingiz uchun tashakkur!');
-}else{
-	$out = array( 'fail' => 'false', 'message' => 'aniq bilmadim lekin bu xato');
+if ($url && $hato_soz && $tuzailgan) {
+    $mysqli->query("INSERT INTO users (url,hato_soz,tuzailgan) VALUES('$url', '$hato_soz', '$tuzailgan')");
+    $out = ['success' => 'true', 'message' => 'ordam berganingiz uchun tashakkur!'];
+} else {
+    $out = ['fail' => 'false', 'message' => 'aniq bilmadim lekin bu xato'];
 }
 header('Content-Type: text/json; charset=utf-8');
 echo json_encode($out);
