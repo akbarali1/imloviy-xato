f4ca1a53a37f220c467bf74c909c8d04d6259f00
Akbarali <39323182+akbarali1@users.noreply.github.com>
2023-01-30 13:15:23 +0500

    update

--- mysql/db.php
+++ mysql/db.php
@@ -1,29 +1,20 @@
 <?php
-// Kod Akbarali tomonidan 2020-yil 10-mart sanasida yasaldi va github'ga qo`yildi
+//Kod Akbarali tomonidan 2020 yil 10-mart sanasida ysaldi va githubga qo`yildi
 // https://github.com/Akbar9705/imloviy-xato
 // Github profil https://github.com/Akbar9705
-// Sayt https://uzhackersw.uz
+//Site https://uzhackersw.uz
+$mysqli = new Mysqli('localhost', 'ajax', '', 'ajax');
+$mysqli->set_charset("utf8");
 
-$url_addres = trim($_POST['url_address']);
-$xato_soz   = trim($_POST['error_text']);
-$tuzatilgan = trim($_POST['fixed_text']);
-if ($url_addres && $xato_soz && $tuzatilgan) {
-    $token          = '<token>';
-    $user_id        = '<telegram_id_raqamingiz>';
-    $xabar          = 'Salom guruh a\'zolari, saytda imloviy xato bo`lgani uchun sizga xabar yozishdi. 
-Sahifa manzili: '.$url_addres.' 
-Xato so`z: '.$xato_soz.'
-Taklif qilingan matn: '.$tuzatilgan.' 
-Xatoni tezda tuzatasizlar deb umid qilaman.';
-    $request_params = ['chat_id' => $user_id, 'text' => $xabar];
-    $request_url    = 'https://api.telegram.org/bot'.$token.'/sendMessage?'.http_build_query($request_params);
-    file_get_contents($request_url);
-    $out = [
-        'success' => 'true',
-        'message' => 'Yordam berganingiz uchun tashakkur!<br>Tez orada ushbu imloviy xatoni tuzatamiz.',
-    ];
+$url        = trim($_POST['url_address']);
+$error_text = trim($_POST['error_text']);
+$fixed_text = trim($_POST['fixed_text']);
+
+if ($url && $error_text && $fixed_text) {
+    $mysqli->query("INSERT INTO users (url,hato_soz,tuzailgan) VALUES('$url', '$error_text', '$fixed_text')");
+    $out = ['success' => 'true', 'message' => 'ordam berganingiz uchun tashakkur!'];
 } else {
-    $out = ['fail' => 'false', 'message' => 'Aniq bilmadim, lekin bu xato.'];
+    $out = ['fail' => 'false', 'message' => 'aniq bilmadim lekin bu xato'];
 }
 header('Content-Type: text/json; charset=utf-8');
 echo json_encode($out);
--- tgbot/db.php
+++ tgbot/db.php
@@ -1,20 +1,28 @@
 <?php
-//Kod Akbarali tomonidan 2020 yil 10-mart sanasida ysaldi va githubga qo`yildi
+// Kod Akbarali tomonidan 2020-yil 10-mart sanasida yasaldi va github'ga qo`yildi
 // https://github.com/Akbar9705/imloviy-xato
 // Github profil https://github.com/Akbar9705
-//Site https://uzhackersw.uz
-$mysqli = new Mysqli('localhost', 'ajax', '', 'ajax');
-$mysqli->set_charset("utf8");
-
-$url       = trim($_POST['url_address']);
-$hato_soz  = trim($_POST['error_text']);
-$tuzailgan = trim($_POST['fixed_text']);
-
-if ($url && $hato_soz && $tuzailgan) {
-    $mysqli->query("INSERT INTO users (url,hato_soz,tuzailgan) VALUES('$url', '$hato_soz', '$tuzailgan')");
-    $out = ['success' => 'true', 'message' => 'ordam berganingiz uchun tashakkur!'];
+// Sayt https://uzhackersw.uz
+$url_addres = trim($_POST['url_address']);
+$error_text = trim($_POST['error_text']);
+$fixed_text = trim($_POST['fixed_text']);
+if ($url_addres && $error_text && $fixed_text) {
+    $token          = '<BOT_TOKEN>';
+    $user_id        = '<ADMIN_TELEGRAM_ID>';
+    $message        = '<b>Salom guruh a\'zolari, saytda imloviy xato bo`lgani uchun sizga xabar yozishdi.</b>'.PHP_EOL;
+    $message        .= '<b>Sahifa manzili:</b> <code>'.$url_addres.'</code>'.PHP_EOL;
+    $message        .= '<b>Xato so`z:</b> <u>'.$error_text.'</u>'.PHP_EOL;
+    $message        .= '<b>Taklif qilingan matn:</b> <u>'.$fixed_text.'</u>'.PHP_EOL;
+    $message        .= 'Xatoni tezda tuzatasizlar deb umid qilaman.';
+    $request_params = ['chat_id' => $user_id, 'text' => $message, 'parse_mode' => 'HTML'];
+    $request_url    = 'https://api.telegram.org/bot'.$token.'/sendMessage?'.http_build_query($request_params);
+    file_get_contents($request_url);
+    $out = [
+        'success' => true,
+        'message' => 'Yordam berganingiz uchun tashakkur!<br>Tez orada ushbu imloviy xatoni tuzatamiz.',
+    ];
 } else {
-    $out = ['fail' => 'false', 'message' => 'aniq bilmadim lekin bu xato'];
+    $out = ['success' => false, 'message' => 'Aniq bilmadim, lekin bu xato.'];
 }
 header('Content-Type: text/json; charset=utf-8');
 echo json_encode($out);
